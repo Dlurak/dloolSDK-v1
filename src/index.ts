db3d3010f674f0b09f9a911dde6c61194f58f3d7
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-10-08 21:52:43 +0200

    Add JSDoc comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { Config } from './resources/base';
 import { Homework } from './resources/homework';
 
+/**
+ * Dlool class
+ * @class
+ * @classdesc The main class for the Dlool SDK
+ * @param {Config} config - The configuration object
+ * @example
+ *  const dlool = new Dlool({
+ *     baseUrl: 'https://dlool-backend.onrender.com',
+ *  })
+ */
 export class Dlool {
+    /**
+     * The homework instance associated with the Dlool instance
+     * @type {Homework}
+     * @class
+     */
     homework: Homework;
 
+    /**
+     * Creates a new Dlool instance
+     *
+     * @constructor
+     * @constructs Dlool
+     * @param {{ baseUrl: string } config - The configuration for the Dlool instance
+     */
     constructor(config: Config) {
         this.homework = new Homework(config);
     }
--- src/resources/homework/index.ts
+++ src/resources/homework/index.ts
@@ -7,7 +7,17 @@ import {
     getAllHomeworkResponse,
 } from './types';
 
+/**
+ * Homework class
+ * @classdesc The homework class for the Dlool SDK. On this class, you can run operations related to homework
+ * @class
+ */
 export class Homework extends BaseResource {
+    /**
+     * A function to get all the homework for a specific class
+     * @param {{ class: string; school: string }} options - The options for the getAllHomework method
+     * @returns The response from the API
+     */
     getAllHomework(options: { class: string; school: string }) {
         const url = `${this.baseUrl}/homework/all?class=${options.class}&school=${options.school}`;
         const response = fetch(url).then((res) => res.json()) as Promise<getAllHomeworkAPIResponse>;
